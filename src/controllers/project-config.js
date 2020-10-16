8c7a0c6479ef62272216af4c3c09cf051e569fca
Tiago Boeing <contato@tiagoboeing.com>
2020-10-16 01:17:30 -0300

    Remove log from getProjectConfig method

--- src/controllers/project-config.js
+++ src/controllers/project-config.js
@@ -1,7 +1,6 @@
 const PROJECT_CONFIG_FILE = 'config.js'
 
 export function getProjectConfig (options) {
-  console.log(options)
   const projectConfigs = require(`${options.templateDirectory}/${PROJECT_CONFIG_FILE}`)
   return projectConfigs
 }
